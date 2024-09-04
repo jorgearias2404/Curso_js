06bad59cead267dd1e624c967eaa997e9e3b8005
JORGOX <ja549655@gmail.com>
2024-09-04 10:14:46 -0400

    Datos elementales de js

---
+++ Practica_js.js
@@ -0,0 +1,45 @@
+console.log("Hola mundo ya ando en js >:)")
+
+/*ANOTOMIA DE UNA VARIABLE */
+let caja = "Es mi nombre";
+let Numeros = 1234;
+const SoyUnaVariableConstante = 243;
+
+/*TIPOS DE DATOS */
+/* PRIMITIVOS*/ 
+//string
+let nombre = "Juan";
+let string1 = "Pepito estudia"
+let string2 = 'Pepito sigue estudiando'
+let string3 =`${string1} esta feliz juntoa ${string2}`
+//number
+let edad = 25;
+//bool
+let estaCasado = false;
+//null
+let estaCasado2 = null;
+//undefind
+let noDefinido =undefined;
+//symbol
+let simbolo = Symbol("mi simbolo");
+//bigint
+let NumeroGrande = 2n;
+
+
+/*COMPLEJOS*/
+//object
+let carro = {
+    marca: "Tesla",
+    modelo: "Modelo 3"
+    
+}
+//array
+let frutas = ["manzana","peras","uvas"];
+//function
+function saludar(){}
+
+
+
+
+
+
